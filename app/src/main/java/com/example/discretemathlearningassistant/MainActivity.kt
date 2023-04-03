163f5b17f772564b0a974810b3df36149fe72d9e
vlash0505 <shupliakovvl@gmail.com>
2023-04-03 22:58:14 +0300

    [ui] preview and catalogue activities skeleton

--- app/src/main/java/com/example/discretemathlearningassistant/MainActivity.kt
+++ app/src/main/java/com/example/discretemathlearningassistant/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.discretemathlearningassistant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnExplore: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnExplore = findViewById(R.id.btnExplore)
+        btnExplore.setOnClickListener {
+            val intent = Intent(this, CatalogueActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
